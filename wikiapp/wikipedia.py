b7f5310d4171455a8050286aaaefa5c799a82ded
Arnit Biswal <yudhistirabiswal@Joes-MacBook-Pro-2.local>
2024-05-28 12:20:38 -0400

    formatting

--- wikiapp/wikipedia.py
+++ wikiapp/wikipedia.py
@@ -1,15 +1,16 @@
-import requests
 import click
+import requests
 
-API_URL = 'https://{language}.wikipedia.org/api/rest_v1/page/random/summary'
+API_URL = "https://{language}.wikipedia.org/api/rest_v1/page/random/summary"
+
 
 def random_page(language="en"):
- """ Get a random page from wikipedia"""
- try:
-     with requests.get(API_URL.format(language=language)) as response:
-          response.raise_for_status()
-          data = response.json()
-     return data
- except requests.RequestException as error:
-    message = str(error)
-    raise click.ClickException(message) from error
+    """Get a random page from wikipedia"""
+    try:
+        with requests.get(API_URL.format(language=language), timeout=10) as response:
+            response.raise_for_status()
+            data = response.json()
+        return data
+    except requests.RequestException as error:
+        message = str(error)
+        raise click.ClickException(message) from error
